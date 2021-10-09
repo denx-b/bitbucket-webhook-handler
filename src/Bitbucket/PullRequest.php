557dd6474ddb196136cc610228ee113b59128258
Sorokin Aleksey <a.sorokin@consult-info.ru>
2021-10-09 22:41:08 +0200

    Добавлено получение комментариев из пулл реквеста

--- src/Bitbucket/PullRequest.php
+++ src/Bitbucket/PullRequest.php
@@ -9,6 +9,9 @@ class PullRequest extends Base
     /** @var array */
     protected $pullRequest = [];
 
+    /** @var array */
+    protected $comment = [];
+
     /**
      * @throws Exception
      */
@@ -22,6 +25,10 @@ class PullRequest extends Base
 
         if (array_key_exists('pullrequest', $this->rawData)) {
             $this->pullRequest = $this->rawData['pullrequest'];
+        }
+
+        if (array_key_exists('comment', $this->rawData)) {
+            $this->comment = $this->rawData['comment'];
         }
     }
 
@@ -48,5 +55,25 @@ class PullRequest extends Base
     public function getAuthor(): array
     {
         return $this->getPullRequest()['author'];
+    }
+
+    public function getAuthorNickName(): string
+    {
+        return $this->getAuthor()['nickname'];
+    }
+
+    public function getCommentText(): string
+    {
+        return $this->comment['content']['raw'];
+    }
+
+    public function getCommentInlinePath(): string
+    {
+        return $this->comment['inline']['path'];
+    }
+
+    public function getCommentInlineNumber(): string
+    {
+        return $this->comment['inline']['from'] ?: $this->comment['inline']['to'];
     }
 }
